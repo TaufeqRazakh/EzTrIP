da78efcd5f52ccef25f7833e18b5566fe06872fa
TaufeqRazakh <taufeqmdr@gmail.com>
2026-03-04 20:15:02 -0800

    Added simple train config as .yaml file with tests to read values

---
+++ tests/test_config_yaml.py
@@ -0,0 +1,17 @@
+import yaml
+from pathlib import Path
+
+CONFIG_PATH = Path(__file__).parent / '../configs/config-train.yaml'
+
+def test_config_file_exists():
+	print(CONFIG_PATH)
+	assert CONFIG_PATH.exists(), "Expected config file, found none"
+
+def test_config_file_readable():
+	with CONFIG_PATH.open(mode='r') as f:
+		config = yaml.safe_load(f)
+		assert "epochs" in config, "Expected epochs in config file, missing values"
+		print(config['epochs'])
+		architecture = config['architecture']
+		assert "embedding_dim" in architecture, "Expected velues for architecture, found none"
+		print(architecture)
