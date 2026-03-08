c924a6930b5a08428a6037e44e8ef1445370607d
TaufeqRazakh <taufeqmdr@gmail.com>
2026-03-07 19:12:57 -0800

    Add filename and directory options to the train script

--- src/eztrip/train_model.py
+++ src/eztrip/train_model.py
@@ -2,21 +2,28 @@ import argparse
 from ase.io import read
 from pathlib import Path
 import torch
+from torch_geometric.loader import DataLoader
 
 
-def main(training_dir, xyz_filename):
-    xyz_path = training_dir / xyz_filename
-    training_structures = read(xyz_path, index=':')
+def main(xyz_filename):
+    training_structures = read(xyz_filename, index=':')
     print(f"Number of training structures: {len(training_structures)}")
 
     forces = [torch.tensor(structure.get_forces()) for structure in training_structures]
 
     energies = [torch.tensor(structure.get_total_energy()) for structure in training_structures]
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a model on molecular data.')
-    parser.add_argument('training_dir', type=Path, help='Path to the directory containing the training data.')
-    parser.add_argument('xyz_filename', type=str, help='Name of the .xyz or trajectory file.')
+    parser.add_argument('-d', '--training_dir', type=Path, 
+                        help='Path to the directory containing the training data.')
+    parser.add_argument('-f', '--filename', type=str, 
+                        help='Name of the .xyz or trajectory file.')
     args = parser.parse_args()
-    
-    main(args.training_dir, args.xyz_filename)
+    if args.training_dir:
+        posix_paths = args.training_dir.glob('*.xyz')
+        for filename in sorted(posix_paths):
+            main(filename)
+    else:
+        main(args.filename)
